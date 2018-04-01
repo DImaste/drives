18fa343277ed57161c3fae9418ed88b89998a49b
Dmitry <pavluhin_dima@mail.ru>
2018-04-02 01:10:30 +0500

    realized signature comparing with vectors

---
+++ __recovery/SearchThread.cpp
@@ -0,0 +1,163 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+#include "FileSystem.h"
+#include "SearchThread.h"
+#include "IteratorThread.h"
+#include "Main.h"
+#include <string>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Important: Methods and properties of objects in VCL can only be
+//   used in a method called using Synchronize, for example:
+//
+//      Synchronize(&UpdateCaption);
+//
+//   where UpdateCaption could look like:
+//
+//      void __fastcall SearchThread::UpdateCaption()
+//      {
+//        Form1->Caption = "Updated in a thread";
+//      }
+
+// this file contains reading thread
+
+//---------------------------------------------------------------------------
+
+__fastcall SearchThread::SearchThread(BYTE *dataBufferPtr, int clusterSize, bool CreateSuspended)
+	: TThread(CreateSuspended)
+{
+	FreeOnTerminate = true;
+
+	// multithread events
+	BufferReadyEvent  = new TEvent(NULL, true, false,"",false);
+	BufferCopiedEvent = new TEvent(NULL, true, false,"",false);
+
+	//BufferAccessCS = new TCriticalSection;
+
+	CurrentCluster = 0;
+	ClusterSize = clusterSize;
+	OutBufferPtr = dataBufferPtr;
+	DataBuffer = new BYTE[clusterSize];
+}
+//---------------------------------------------------------------------------
+void __fastcall SearchThread::Execute()
+{
+	while(true)
+	{
+		// Ждать, пока не будет подготовлен буфер для копирования
+		if(BufferReadyEvent->WaitFor(WaitDelayMs) == wrSignaled)
+		{
+			//if(BufferAccessCS->TryEnter())
+			if(true)
+			{
+				// Скопировать данные
+				CopyData();
+
+				// Отпустить буфер
+				BufferReadyEvent->ResetEvent();
+				BufferCopiedEvent->SetEvent();
+
+				// Запустить поиск
+				SearchData();
+				//BufferAccessCS->Leave();
+			}
+		}
+		if(Terminated) break;
+	}
+	// Удалить события
+	delete BufferReadyEvent;
+	delete BufferCopiedEvent;
+	//delete BufferAccessCS;
+
+  // Удалить буфер
+	delete[] DataBuffer;
+	Synchronize(&CompleteSearch);
+}
+//---------------------------------------------------------------------------
+
+void SearchThread::SetCurrentCluster(int Cluster)
+ {
+	CurrentCluster = Cluster;
+
+ }
+
+
+//---------------------------------------------------------------------------
+void SearchThread::CopyData()
+{
+	memcpy(DataBuffer, OutBufferPtr, ClusterSize);
+}
+
+//---------------------------------------------------------------------------
+void SearchThread::SearchData()
+{
+    vector<const char *> signatures;
+
+   signatures.push_back("\x49\x44\x33"); //mp3
+   signatures.push_back("\x21\x3C\x61\x72\x63\x68\x3E"); //deb
+   signatures.push_back("\x89\x50\x4E\x47\x0D\x0A\x1A\x0A"); //png    89 50 4E 47 0D 0A 1A 0A
+   signatures.push_back("\x37\x7A\xBC\xAF\x27\x1C"); //7zip    37 7A BC AF 27 1C
+
+   vector<const char *> sign_size;
+
+   sign_size.push_back("3");
+   sign_size.push_back("7");
+   sign_size.push_back("8");
+   sign_size.push_back("6");
+
+   vector<const char *> sign_name;
+
+   sign_name.push_back(".mp3");
+   sign_name.push_back(".deb");
+   sign_name.push_back(".png");
+   sign_name.push_back(".7zip");
+
+
+
+
+   for (int i; i<signatures.size();i++)
+	{
+	// Провести поиск
+		 if ( memcmp( DataBuffer, signatures[i], 4) == 0 )
+		{
+			memcpy( signature, signatures[i], 4);
+			memcpy( extensionFile, ".mp3", 4 );
+			Synchronize(&AddMatch );
+		}
+
+	}
+	// memcmp
+   //	bool matchFound = true;
+
+   /*	if(matchFound)
+	{
+		Synchronize(&AddMatch);
+	} */
+}
+//---------------------------------------------------------------------------
+void __fastcall SearchThread::AddMatch()
+{
+	//PVirtualNode newNode = MainForm->ResultTree->AddChild(MainForm->ResultTree->RootNode);
+
+	PVirtualNode newNode = MainForm->ResultTree->AddChild(MainForm->ResultTree->RootNode);
+	FileTree* nodeData = (FileTree*)MainForm->ResultTree->GetNodeData(newNode);
+	nodeData-> currentCluster = CurrentCluster;
+	memcpy(nodeData->signature, signature, 4);
+	memcpy(nodeData->extensionFile, extensionFile, 4);
+
+}
+
+
+//---------------------------------------------------------------------------
+void __fastcall SearchThread::CompleteSearch()
+{
+	Application->MessageBoxW(L"Поиск завершен!", L"", MB_OK);
+}
---
+++ __recovery/SearchThread.h
@@ -0,0 +1,65 @@
+//---------------------------------------------------------------------------
+
+#ifndef SearchThreadH
+#define SearchThreadH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <System.SyncObjs.hpp>
+#include <string>
+#include <vector>
+#include <list>
+
+using namespace std;
+//---------------------------------------------------------------------------
+// Критическая секция добавления результатов поиска
+	//TCriticalSection *BufferAccessCS;
+//---------------------------------------------------------------------------
+const int WaitDelayMs = 2000;
+//---------------------------------------------------------------------------
+
+typedef struct
+{
+	__int64 currentCluster;
+	BYTE signature[5];
+	BYTE extensionFile[5];
+} FileTree;
+
+
+class SearchThread : public TThread
+{
+private:
+	int ClusterSize;
+	BYTE *OutBufferPtr;
+	BYTE *DataBuffer;
+	int CurrentCluster;
+	BYTE signature[5];
+	BYTE extensionFile[5];
+
+
+	void CopyData();
+	void SearchData();
+	void __fastcall AddMatch();
+	void __fastcall CompleteSearch();
+
+
+protected:
+	void __fastcall Execute();
+
+
+
+
+
+
+public:
+	__fastcall SearchThread(BYTE *dataBufferPtr, int clusterSize, bool CreateSuspended);
+	 void SetCurrentCluster(int Cluster);
+
+	// События, используемые для синхронизации
+	TEvent *BufferReadyEvent;
+	TEvent *BufferCopiedEvent;
+
+
+
+};
+//---------------------------------------------------------------------------
+#endif
